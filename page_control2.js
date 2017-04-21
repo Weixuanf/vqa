3df9f8a3affdbdd0a6e0bdb29cab7c4a07d0a009
Weixuan Fu <weixuanf@outlook.com>
2017-04-21 13:18:34 -0500

    Update after meeting with prof.
    Add an overview page, center the draw-box, add examples

--- page_control2.js
+++ page_control2.js
@@ -4,33 +4,45 @@
 
 document.getElementById('nextBtn').addEventListener('click',renderNextPage);
 document.getElementById('submitButton').addEventListener('click',formSubmit);
-//hide and show main instructions
-document.getElementById("hide-btn").addEventListener("click", function () {
-    document.getElementById("main_instructions").style.display = "none";
-});
-document.getElementById("show-btn").addEventListener("click", function () {
-    document.getElementById("main_instructions").style.display = "block";
-});
+
+var pageNum = document.getElementById('pageNum');
+function getPageNum() {
+    return parseInt(pageNum.innerHTML);
+}
 function renderNextPage() {
-    if (notDrawn) {
-        alert("You must give your bounding box before you can go the the next page!");
-        return;
+    var page = getPageNum();
+    if (page==1) {//currently at the 1st page
+        document.getElementById("page-1").style.display = "none";
+        document.getElementById("page-2").style.display = "block";
+        document.getElementById("questionBox").style.display = "block";
+        init();//init the drawing canvas (this funtion is in drawbox.js)
+
+        document.getElementById("step-cell-1").classList.add("active-nav");//change the step nav active cell
+        document.getElementById("step-cell-0").classList.remove("active-nav");//change the step nav active cell
     }
-    document.getElementById("myInput").type = "text";
-    document.getElementById("myInput").focus();
-    document.getElementById('nextBtn').style.display = 'none'; //hide the NEXT button if it's the last page
-    document.getElementById('submitButton').type = "button"; //display the SUBMIT button at the last page
+    if (page==2) {//currently at the 2nd page
+        if (notDrawn) {
+            alert("You must give your bounding box before you can go the the next page!");
+            return;
+        }
+        document.getElementById("myInput").type = "text";
+        document.getElementById("myInput").focus();
+        document.getElementById('nextBtn').style.display = 'none'; //hide the NEXT button if it's the last page
+        document.getElementById('submitButton').type = "button"; //display the SUBMIT button at the last page
 
-    document.getElementById("main-instruction-1").style.display = "none";
-    document.getElementById("main-instruction-2").style.display = "block";
+        //change the location of the canvas to the left-bar on the 3rd page (cur and paste in dom)
+        document.getElementById('leftBar').appendChild(document.getElementById('draw-board'));
+        document.getElementById("page-2").style.display = "none";
+        document.getElementById("page-3").style.display = "block";
 
-    document.getElementById("task1inst").style.display = 'none';
-    document.getElementById("task2inst").style.display = 'block';
-    document.getElementById("confidenceRadio").style.display = 'block'; //display the radio buttons of confidence
-    document.getElementById("step-cell-2").classList.add("active-nav");//change the step nav active cell
-    document.getElementById("step-cell-1").classList.remove("active-nav");//change the step nav active cell
+        document.getElementById("confidenceRadio").style.display = 'block'; //display the radio buttons of confidence
+        document.getElementById("step-cell-2").classList.add("active-nav");//change the step nav active cell
+        document.getElementById("step-cell-1").classList.remove("active-nav");//change the step nav active cell
 
-    window.scrollTo(0, 0); //scoll to the top of the page
+        window.scrollTo(0, 0); //scoll to the top of the page
+    }
+    pageNum.innerHTML = ++page;
+
 }
 
 //deal with on submit the form
